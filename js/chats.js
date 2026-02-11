74885533306bb7ac3c9f37eef454cc8eae129d19
Lethabo Maepa <lethabo.maepa@boxfusion.io>
2026-02-11 18:43:43 +0200

    Completed the real-time functionality of private chat, error handling to avoid multiple private chats with the same people

--- js/chats.js
+++ js/chats.js
@@ -9,14 +9,41 @@ const searchInput = document.getElementById("searchInput");
 const middleView = document.getElementById("middleView");
 const mainView = document.getElementById("mainView");
 const messagesView = document.getElementById("messagesView");
+const chatIdElement = document.createElement("input");
 const sessionUser = JSON.parse(sessionStorage.getItem("session"));
 const chatStore = new Store("chats");
 const userStore = new Store("users");
 
 
 //event listeners
-document.addEventListener("load", displayChats());
+window.addEventListener("load", () => {
+    //display the chats first
+    displayChats()
+    //check if there is a "c" searchParam
+    const chatId = new URL(location.href).searchParams.get("c");
+
+    //if not empty, select that chat
+    if (chatId) {
+        showSelectedChat(chatId);
+    }
+    
+});
 searchInput.addEventListener("change", (e) => getUsers(searchInput.value));
+//listen for events on localstorage chats key
+window.addEventListener("storage", (e) => {
+    if (e.key == "chats") {
+        displayChats();
+        const affectedChat = JSON.parse(e.newValue).at(-1);
+        const chatId = chatIdElement.value;
+        //if it is the currently opened chat, update the messagesView
+        if (affectedChat.id == chatId) {
+            renderMessages(null, true, affectedChat.messages.at(-1));
+        }
+    }
+})
+
+
+
 
 
 
@@ -42,11 +69,15 @@ function displayChats() {
         const user = userStore.getAll().find(u => u.id == userId);
         item.addEventListener("click", () => showSelectedChat(chat.id))
         item.innerHTML =
-            `<div id='${user.username}' class='chat-list-item'>
+            `<div id='${user.username}' class='chat-list-item ${chatIdElement.value == chat.id ? "bg-background": ""}'>
                 <img class='avatar' src='../assets/images/avatar.jpg'/>
                 <span>
                     <p>${user.firstName} ${user.surname} ${sessionUser.id == user.id ? "(You)":""}</p>
-                    <p class='text-grey text-xs'>Message</p>
+                    <p class='text-grey text-xs'>
+                        ${chat.messages.at(-1)?.senderId == sessionUser.id ? "You" : user.firstName}:
+                        ${chat.messages.at(-1)?.content ?? ""}
+                    </p>
+                    <p class='text-grey text-xs'>${chat.messages.at(-1)?.time ?? ""}</p>
                 </span>
             </div>`
         list.appendChild(item);
@@ -94,10 +125,14 @@ window.toggleView = toggleView;
 window.sendMessage = sendMessage;
 
 function showSelectedChat(chatId) {
+    //save to the chatIdElement that is not yet on the page
+    chatIdElement.value = chatId;
+
+    //get the chat
     const chat = chatStore.getAll().find(c => c.id == chatId);
-    const userId = chat.users.find(id => id != sessionUser.id);
-    const user = userStore.getAll().find(u => u.id == userId);
     if (chat) {
+        const userId = chat.users.find(id => id != sessionUser.id);
+        const user = userStore.getAll().find(u => u.id == userId);
         //toggle the view on mobile
         toggleView();
         //show the person on the main view
@@ -119,12 +154,13 @@ function showSelectedChat(chatId) {
             <button class="bg-primary" onclick="sendMessage('${chat.id}')">Send</button>
         `
 
+        displayChats();
         renderMessages(chat);
         
     }
     else {
         const alert = new Alert();
-        alert.show("error", "An error has occured, please reload page and try again");
+        alert.show("error", "This chat is inaccessible, it might have been deleted, or it never existed");
     }
 }
 
@@ -133,6 +169,7 @@ function renderMessages(chat, append = false, message = null) {
     /**
      * Renders messages on the DOM
      * if append is true, message must be an object, it will append message into the dom instead of replacing all elements
+     * chat is optional if append is true, can be set to null
      */
 
     if (append && !message) {
@@ -151,6 +188,8 @@ function renderMessages(chat, append = false, message = null) {
         </div>
         `
         messagesView.appendChild(element);
+        //scroll down
+        messagesView.scrollTop = messagesView.scrollHeight;
         return;
     }
     //if no messages are there yet
@@ -183,6 +222,7 @@ function renderMessages(chat, append = false, message = null) {
 
     //render the messages
     messagesView.replaceChildren(...messages);
+    messagesView.scrollTop = messagesView.scrollHeight;
     
 }
 
@@ -193,10 +233,15 @@ function sendMessage(chatId) {
         alert.show("error", "Cannot send empty message");
     }
     const message = new Message(chatId, sessionUser.id, text);
-    const chat = chatStore.getAll().find(c => c.id == chatId);
-
+    //get the chats
+    const chats = chatStore.getAll();
+    const chat = chats.find(c => c.id == chatId);
+    //push new message
     chat.messages.push(message);
-    renderMessages(chat,true,message);
+    localStorage.setItem("chats", JSON.stringify(chats));
+    //render message
+    renderMessages(chat, true, message);
+    displayChats();
     document.getElementById("chatInput").value = "";
 }
 
